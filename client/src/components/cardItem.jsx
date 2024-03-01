7224d9933e8099f9f914f236e1a84561ab27f8fc
meownerPidoru <pidory2288@gmail.com>
2024-03-01 19:32:11 +0300

    aaa

--- client/src/components/cardItem.jsx
+++ client/src/components/cardItem.jsx
@@ -1,5 +1,6 @@
 import '../App.css';
 import { Text, Box, Flex, Card, CardHeader, CardBody, CardFooter, Image, Stack, Heading, Button } from '@chakra-ui/react'
+import { homeRoute } from '../utils/consts';
 import Porsche911 from '../images/porsche911.png'
 
 const CardItem = () =>{
@@ -19,16 +20,17 @@ const CardItem = () =>{
                         src={Porsche911}
                         width={'300px'}
                         alt='porsche911'
+                        borderRadius={'10%'}
                 ></Image>
                 <Box
                     width={'100%'}
                     textAlign={'center'}
                 >
-                    <Heading fontSize={'30px'}>Porsche 911</Heading>
+                    <Heading fontSize={'30px'}>Cir</Heading>
                 </Box>
                 <Box>
                     <Text>
-                        <b>Feuteres:</b> <i>Power 385hp | Weight 1700kg | Max speed 303km/h</i>
+                        <b>Sort:</b> <i> | higher 1700kg | taste speed 303km/h</i>
                     </Text>
                 </Box>
                 <Flex
--- client/src/pages/authPage.jsx
+++ client/src/pages/authPage.jsx
@@ -1,5 +1,6 @@
 import NavBarSh from '../components/NavBarSh';
-import { Flex } from '@chakra-ui/react';
+import { Center, Flex, Text, Box, InputGroup,InputLeftAddon, Input, InputRightAddon, Button, Checkbox, Link} from '@chakra-ui/react';
+import { ShopRoute, HomeRoute, AuthRoute } from '../utils/consts';
 const AuthPage = () =>{
     return(
         <Flex
@@ -17,9 +18,48 @@ const AuthPage = () =>{
                 <Flex
                     boxShadow={'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;'}
                     width={'300px'}
-                    height={'300px'}
+                    height={'400px'}    
+                    justify={'center'}
+                    flexWrap={'wrap'}
+                >   <Box
+                     width={'100%'}
+                     textAlign={'center'}
+                     justify={'space-around'}>
+                    <Text textAlign={'center'} margin={'30px 0px 30px 0px'} fontSize='4xl' > <b>Sing in</b></Text>             
+
+                    <Checkbox defaultChecked margin={'0px 0px 20px 0px'}>ты Беларус?</Checkbox>
+                   
+                    <InputGroup w={'100%'} width={'250px'} paddingLeft={'50px'}  >
+                            <InputLeftAddon>
+                            +375
+                            </InputLeftAddon>
+                            <Input type='tel' placeholder='phone num' />
+                        </InputGroup>
+
+
+                        <InputGroup size='sm' w={'100%'} width={'250px'} paddingLeft={'50px'} marginTop='10px' >
+                            <Input placeholder='e-mail' />
+                            <InputRightAddon>
+                            @gmail.com
+                            </InputRightAddon>
+                        </InputGroup>
+                       < InputGroup size='sm' w={'100%'} width={'250px'} paddingLeft={'50px'} marginTop='10px'>
+                            <Input placeholder='password' />
+                        </InputGroup>
+                        <Button colorScheme='teal' variant='solid' marginTop='10px'>
+                        Sing In
+                        </Button>
+                        <Link
+                            >
+                        <Box>
+                        <Button to={HomeRoute} colorScheme='teal' variant='link' marginTop='10px'>
+                            уходи.
+                        </Button>
+                        </Box>
+                        </Link>
+                    </Box>
                     
-                ></Flex>
+                </Flex>
             </Flex>
         </Flex>
        

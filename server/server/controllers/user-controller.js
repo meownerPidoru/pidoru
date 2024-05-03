5d01ba531970c5315504e7540060974eb328449d
meownerPidoru <pidory2288@gmail.com>
2024-05-03 19:29:09 +0300

    meow

--- server/server/controllers/user-controller.js
+++ server/server/controllers/user-controller.js
@@ -15,11 +15,20 @@ class UserController {
   }
   async login(req, res, next) {
     try {
+      const activationLink = req.params.link;
+      await userService.activate(activationLink);
+      return res.redirect(process.env.LOCAL_CLIENT_URL)
     } catch (e) {}
   }
   async logout(req, res, next) {
     try {
-    } catch (e) {}
+      const( email, password) = req.body();
+      const userData = await userService.login(email, password)
+      res.cookie('refreshToken', userData.refreshToken,{maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, })
+        return res.json(userData);
+    } catch (e) {
+      console.log(e)
+    }
   }
   async activate(req, res, next) {
     try {
--- server/server/service/user-service.js
+++ server/server/service/user-service.js
@@ -1,30 +1,46 @@
-const userModel = require("../models/user-model");
-const mailService = require("./mail-service");
-const bcrypt = require("bcrypt");
-const uuid = require("uuid");
-const tokenService = require("./token-service");
-const UserDto = require("../dtos/user-dto");
+const userModel = require('../models/user-model');
+const mailService = require('./mail-service');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const UserDto = require('../dtos/user-dto');
+const tokenService = require('./token-service');
 class UserService {
-  async registration(email, password) {
-    const candidate = await userModel.findOne({ email });
-    if (candidate) {
-      throw new Error("Пользователь уже зарегестрирован");
+    async registration(email, password) {
+        const candidate = await userModel.findOne({ email });
+        if (candidate) {
+            throw new Error('Пользователь уже зарегистрирован');
+        }
+        const hashPassword = await bcrypt.hash(password, 3);
+        const activationLink = uuid.v4();
+        const user = await userModel.create({ email, password: hashPassword, activationLink });
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
+        const userDto = new UserDto(user)
+        const tokens = tokenService.generateTokens({ ...userDto });
+        await tokenService.saveToken(userDto.id, tokens.refreshToken)
+        return { ...tokens, user: userDto }
     }
-    const hashPassword = await bcrypt.hash(password, 3);
-    const activationLink = uuid.v4();
-    const user = await userModel.create({
-      email,
-      password: hashPassword,
-      activationLink,
-    });
-    await mailService.sendActivationMail(email, activationLink);
-    const userDto = new UserDto(user);
-    const tokens = tokenService.generateTokens({ ...userDto });
-    await tokenService.saveToken(userDto.id, tokens.refreshToken);
-    return {
-      ...tokens,
-      user: userDto, //перепись переменной
-    };
-  }
+    async activate(activationLink){
+      const user = await userModel.findOne({activationLink});
+      if(!user){
+        throw new Error("Некорректная ссылка")
+      }
+      user.isActivated = true;
+      await user.save();
+    }
+    async login(email, password){
+      const user = await userModel.findOne({email})
+      if(!user){
+        throw new Error("User not found");
+      }
+      const isPassEquals = await bcrypt.compare(password,user.password)
+      if(!isPassEquals){
+        throw new Error("Невереный пароль");
+      }
+      const userDto = new UserDto(user);
+      const tokens = tokenService.generateTokens({ ...userDto }); 
+      await tokenService.saveToken(userDto.id, tokens.refreshToken); 
+      return { ...tokens, user: userDto }
+    }
+    
 }
 module.exports = new UserService();

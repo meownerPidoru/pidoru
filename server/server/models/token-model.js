8816ec8bd1a137eb32e85285f39fe81b5f0d6453
mui <pidory2288@gmail.com>
2024-05-10 18:48:53 +0300

    delll server

--- server/server/models/token-model.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const TokenModel = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-  refreshToken: { type: String, required: true },
-});
-module.exports = model("Token", TokenModel);
